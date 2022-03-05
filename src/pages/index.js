7c91b03be6270fcc9950333ae56231627587b02a
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-05 21:02:20 +0100

    Imported and exported pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,12 @@
-import Home from "./Home/Home";
-import Carta from "./Carta/Carta";
+import Home from './Home/Home';
+import Carta from './Carta/Carta';
+import LaCocteleria from './LaCocteleria/LaCocteleria';
+import Contacto from './Contacto/Contacto';
+import Login from './Login/Login';
+import Pedidos from './Pedidos/Pedidos';
+import Pedir from './Pedir/Pedir';
+import Register from './Register/Register';
+import Reservar from './Reservar/Reservar';
+import Reservas from './Reservas/Reservas';
 
-export { Home, Carta };
+export { Home, Carta, LaCocteleria, Contacto, Login, Pedidos, Pedir, Register, Reservar, Reservas };
