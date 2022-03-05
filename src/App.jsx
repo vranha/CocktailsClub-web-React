982bbf1b0dcc3f58227c9b1e64c7e2c5be77ab70
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-05 21:01:21 +0100

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import './App.scss';
-import { Home, Carta } from "./pages"
-import { Header, Footer } from "./components"
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Home, Carta, LaCocteleria, Contacto, Login, Pedidos, Pedir, Register, Reservar, Reservas } from './pages';
+import { Header, Footer } from './components';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+
+//fasdfsa
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <h1>Hola</h1>
-      <Router>
-        <Routes>
-          <Route path="*" element={<Navigate replace to="/" />} />
-          <Route path="/">
-                <Route index element={<Home />} />
-                <Route path="carta" element={<Carta />} />
-          </Route>
-        </Routes>
-      </Router>
-    <Footer></Footer>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header></Header>
+            <h1>Hola</h1>
+            <Router>
+                <Routes>
+                    <Route path="*" element={<Navigate replace to="/" />} />
+                    <Route path="/">
+                        <Route index element={<Home />} />
+                        <Route path="carta" element={<Carta />} />
+                        <Route path="laCocteleria" element={<LaCocteleria />} />
+                        <Route path="contacto" element={<Contacto />} />
+                        <Route path="pedir" element={<Pedir />} />
+                        <Route path="pedidos" element={<Pedidos />} />
+                        <Route path="reservar" element={<Reservar />} />
+                        <Route path="reservas" element={<Reservas />} />
+                        <Route path="register" element={<Register />} />
+                        <Route path="login" element={<Login />} />
+                    </Route>
+                </Routes>
+            </Router>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
