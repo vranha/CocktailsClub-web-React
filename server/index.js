fa694eab25fc554e00f47546d6f67e806097756a
Carlos <carlos.casal@bootcamp-upgrade.com>
2022-03-06 14:35:32 +0100

    Product model created

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const connect = require("./database/connection");
 
-const PORT = 4000;
+require("dotenv").config();
+
+const PORT = process.env.PORT;
 
 const server = express();
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+
+connect();
+
+server.options("*", cors()); // include before other routes
+server.use(cors());
 
 server.listen(PORT, () => {
   console.log(`Server running in http://127.0.0.1:${PORT}`);
