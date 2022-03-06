7274b2908e9a0889ae83aaab0a252b772265e5d3
urii urii2 <urioleh@gmail.com>
2022-03-06 11:49:30 +0100

    FEAT: mierda

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
-import './App.scss';
-import { Home, Carta } from "./pages"
-import { Header, Footer } from "./components"
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import "./App.scss";
+import { Home, Carta } from "./pages";
+import { Header, Footer } from "./components";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <h1>Hola</h1>
-      <Router>
-        <Routes>
-          <Route path="*" element={<Navigate replace to="/" />} />
-          <Route path="/">
-                <Route index element={<Home />} />
-                <Route path="carta" element={<Carta />} />
-          </Route>
-        </Routes>
-      </Router>
-    <Footer></Footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header></Header>
+            <h1>Hola</h1>
+            <Router>
+                <Routes>
+                    <Route path="*" element={<Navigate replace to="/" />} />
+                    <Route path="/">
+                        <Route index element={<Home />} />
+                        <Route path="carta" element={<Carta />} />
+                    </Route>
+                </Routes>
+            </Router>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
