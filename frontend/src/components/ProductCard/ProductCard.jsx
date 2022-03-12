ba66e0d94ac3df7b6580a63f6ecad14069b3b94e
Carlos <carlos.casal@bootcamp-upgrade.com>
2022-03-12 12:38:47 +0100

    Product card modified

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -1,5 +1,6 @@
 import styles from "./ProductCard.module.scss";
 import waiterIcon from "../../assets/waiterIcon.png";
+import Button from "react-bootstrap/Button";
 
 export default function ProductCard({ props }) {
   const { name, description, image, price } = props;
@@ -11,7 +12,10 @@ export default function ProductCard({ props }) {
       <div>{name}</div>
       <div className={styles.description}>{description}</div>
       <div>
-        <button className={styles.buttonPrice}>{`${price}€`}</button>
+        <Button className={styles.buttonPrice} variant="outline-dark">
+          {`${price}€`}
+        </Button>
+
         <img className={styles.buttonIcon} src={waiterIcon} alt="" />
       </div>
     </div>
