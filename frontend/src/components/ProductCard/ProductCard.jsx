a40cd5aaa6e016459484cd6b013d5231d118cad7
Carlos <carlos.casal@bootcamp-upgrade.com>
2022-03-12 12:32:31 +0100

    Product card modified

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -1,4 +1,5 @@
 import styles from "./ProductCard.module.scss";
+import waiterIcon from "../../assets/waiterIcon.png";
 
 export default function ProductCard({ props }) {
   const { name, description, image, price } = props;
@@ -8,7 +9,11 @@ export default function ProductCard({ props }) {
         <img className={styles.image} src={image} alt="" />
       </div>
       <div>{name}</div>
-      <div>{description}</div>
+      <div className={styles.description}>{description}</div>
+      <div>
+        <button className={styles.buttonPrice}>{`${price}€`}</button>
+        <img className={styles.buttonIcon} src={waiterIcon} alt="" />
+      </div>
     </div>
   );
 }
