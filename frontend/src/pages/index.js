7d35f1fdaaf765f724f0ec2c48078f3e47a2699d
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-19 19:43:56 +0100

    Added missing s in Bookings and formatted code

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,13 +1,13 @@
-import Home from "./Home/Home";
-import Menu from "./Menu/Menu";
-import Conocenos from "./Conocenos/Conocenos";
-import Login from "./Login/Login";
-import OrderList from "./OrderList/OrderList";
-import Order from "./Order/Order";
-import Register from "./Register/Register";
-import Book from "./Book/Book";
-import Booking from "./Bookings/Bookings";
-import Encuentranos from "./Encuentranos/Encuentranos";
-import Contactanos from "./Contactanos/Contactanos";
+import Home from './Home/Home';
+import Menu from './Menu/Menu';
+import Conocenos from './Conocenos/Conocenos';
+import Login from './Login/Login';
+import OrderList from './OrderList/OrderList';
+import Order from './Order/Order';
+import Register from './Register/Register';
+import Book from './Book/Book';
+import Bookings from './Bookings/Bookings';
+import Encuentranos from './Encuentranos/Encuentranos';
+import Contactanos from './Contactanos/Contactanos';
 
-export { Home, Menu, Conocenos, Login, OrderList, Order, Register, Book, Booking, Encuentranos, Contactanos };
+export { Home, Menu, Conocenos, Login, OrderList, Order, Register, Book, Bookings, Encuentranos, Contactanos };
