2ee1adafdc5672aa4f25511c5e362e62c25ad8d4
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-19 19:44:49 +0100

    Advanced a bit more with the login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -9,15 +9,42 @@ export default function Login() {
         password: '',
     });
 
+    const { email, password } = formData;
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const userData = {
+            email,
+            password,
+        };
+        // dispatch(login(userData));
+        console.log(userData);
+        return userData;
+    };
+
     return (
         <>
-            <Form className="container d-flex flex-column align-items-center">
+            <Form className="container d-flex flex-column align-items-center" onSubmit={onSubmit}>
                 <h1>Vamos a tomar algo</h1>
                 <Form.Group className="mb-3 text-white" controlId="formBasicEmail">
                     <Form.Label>
                         <h3>Correo</h3>
                     </Form.Label>
-                    <Form.Control type="email" placeholder="Introduce tu correo electrónico" />
+                    <Form.Control
+                        type="email"
+                        className="form-control"
+                        // id="email"
+                        name="email"
+                        value={email}
+                        placeholder="Introduce tu correo electrónico"
+                        onChange={onChange}
+                    />
                     <Form.Text className="text-muted">Sin el no podrás entrar.</Form.Text>
                 </Form.Group>
 
@@ -25,7 +52,15 @@ export default function Login() {
                     <Form.Label>
                         <h3>Password</h3>
                     </Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control
+                        type="password"
+                        className="form-control"
+                        // id="password"
+                        name="password"
+                        value={password}
+                        placeholder="Introduce tu Password"
+                        onChange={onChange}
+                    />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
