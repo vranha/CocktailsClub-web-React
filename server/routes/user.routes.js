2d10014df6cec004f49285359e769085e7165886
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-19 19:47:30 +0100

    Commented some lines due to non use for the moment

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, getMe } = require('../controllers/userController');
-const { protect } = require('../middlewares/authMiddleware'); // Falta determinar creacion de carpeta o uso de una existente para el archivo
+const { registerUser, loginUser } = require('../controllers/userController');
+// const { registerUser, loginUser, getMe } = require('../controllers/userController');
+// const { protect } = require('../middlewares/auth.middleware'); // Falta determinar creacion de carpeta o uso de una existente para el archivo
 
-router.post('/', registerUser);
+router.post('/registro', registerUser);
 router.post('/login', loginUser);
-router.get('/me', protect, getMe);
+// router.get('/me', protect, getMe);
 
 module.exports = router;
