4d55a5c58bd0140d8bbd4e569891080bc4b15778
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-20 20:57:49 +0100

    Added get route for the users but not working yet

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const User = require('../models/user');
 const router = express.Router();
 
 const { registerUser, loginUser } = require('../controllers/userController');
 // const { registerUser, loginUser, getMe } = require('../controllers/userController');
 // const { protect } = require('../middlewares/auth.middleware'); // Falta determinar creacion de carpeta o uso de una existente para el archivo
+
+router.get('/', async (req, res, next) => {
+    return User.find().then((users) => {
+        return res.status(200).json(users);
+    });
+});
 
 router.post('/registro', registerUser);
 router.post('/login', loginUser);
