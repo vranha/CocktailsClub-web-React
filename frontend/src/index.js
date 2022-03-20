258d01d7d46e9195342d59f5be13b57957f8fa84
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-20 20:56:24 +0100

    Implemented redux for the login (not functional yet)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { login, reset } from '../../features/authSlice';
+import { toast } from 'react-toastify';
 
+// BOOTSTRAP IMPORTS
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
@@ -10,6 +15,22 @@ export default function Login() {
     });
 
     const { email, password } = formData;
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user, isError, isSuccess, message } = useSelector((state) => state.auth);
+    // const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+        if (isSuccess || user) {
+            navigate('/');
+        }
+        dispatch(reset());
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -23,9 +44,9 @@ export default function Login() {
             email,
             password,
         };
-        // dispatch(login(userData));
-        console.log(userData);
-        return userData;
+        dispatch(login(userData));
+        // console.log(userData);
+        // return userData;
     };
 
     return (
