8e3c99bc3ff97c5820b6c0befd182a0cc8cab656
Daniel Gonzales <dani.gt75.dg@gmail.com>
2022-03-21 00:36:41 +0100

    FEAT: page register added

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,9 +1,49 @@
 import styles from './Register.module.scss'
 
 export default function Register() {
+
+    const INITIAL_STATE = {
+        nombre: '',
+        apellido: '',
+        username: '',
+        password: '',
+        movil: ''    
+    }
+
+    const submitForm = (ev) => {
+        ev.preventDefault();
+    }
+
     return (
         <div className={styles.container}>
-            <h2>Register</h2>
+            <div className={styles.formContainer}>
+                <h1>Regístrate</h1>
+                <form className="row g-3" onSubmit={submitForm}>
+                    <div className="col-md-12">
+                        <label for="validationDefault01" className="form-label">First name</label>
+                        <input type="text" className="form-control" id="validationDefault01" placeholder='Nombre...' required/>
+                    </div>
+                    <div className="col-md-12">
+                        <label for="validationDefault02" className="form-label">Last name</label>
+                        <input type="text" className="form-control" id="validationDefault02" placeholder='Apellido...' required/>
+                    </div>
+                    <div className="col-md-12">
+                        <label for="inputEmail4" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="inputEmail4"/>
+                    </div>
+                    <div className="col-md-12">
+                        <label for="inputPassword4" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="inputPassword4"/>
+                    </div>  
+                    <div className="col-md-12">
+                        <label for="inputCity" className="form-label">Móvil</label>
+                        <input type="text" className="form-control" id="inputMobile"/>
+                    </div>
+                    <div className="col-12">
+                        <button type="submit" className="btn btn-primary">Sign in</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
