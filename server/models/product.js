dd19195c0bf7bcaab36d2a21366421f41a5c6e03
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-20 21:02:31 +0100

    Formatted code

--- server/models/product.js
+++ server/models/product.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const product = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true },
-  name: { type: String, required: true },
-  type: { type: String, enum: ["cocktail", "appetizer"] },
-  price: { type: Number, required: true },
-  image: { type: String, required: true },
-  description: { type: String, required: true },
-  others: {
-    vegan: { type: Boolean, default: false },
-    spicy: { type: Boolean, default: false },
-  },
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    type: { type: String, enum: ['cocktail', 'appetizer'] },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    others: {
+        vegan: { type: Boolean, default: false },
+        spicy: { type: Boolean, default: false },
+    },
 });
 
-const Product = mongoose.model("product", product);
+const Product = mongoose.model('product', product);
 
 module.exports = Product;
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,40 +1,40 @@
-const express = require("express");
-const Product = require("../models/product");
+const express = require('express');
+const Product = require('../models/product');
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  return Product.find().then((products) => {
-    return res.status(200).json(products);
-  });
+router.get('/', async (req, res, next) => {
+    return Product.find().then((products) => {
+        return res.status(200).json(products);
+    });
 });
 
-router.post("/new", async (req, res, next) => {
-  const { name, type, price, image, description, others } = req.body;
-  let id = 1;
-  const productCounter = await Product.countDocuments();
-  const productExist = await Product.findOne({ name });
+router.post('/new', async (req, res, next) => {
+    const { name, type, price, image, description, others } = req.body;
+    let id = 1;
+    const productCounter = await Product.countDocuments();
+    const productExist = await Product.findOne({ name });
 
-  productCounter ? (id = productCounter + 1) : id;
+    productCounter ? (id = productCounter + 1) : id;
 
-  if (productExist) {
-    res.status(400);
-    res.json("Product already in db");
-  } else if (type != "cocktail" && type != "appetizer") {
-    res.status(400);
-    res.json("Product type has to be cocktail or appetizer");
-  } else {
-    Product.create({
-      id,
-      name,
-      type,
-      price,
-      image,
-      description,
-      others,
-    });
-    res.status(200);
-    res.json("Product created in db");
-  }
+    if (productExist) {
+        res.status(400);
+        res.json('Product already in db');
+    } else if (type != 'cocktail' && type != 'appetizer') {
+        res.status(400);
+        res.json('Product type has to be cocktail or appetizer');
+    } else {
+        Product.create({
+            id,
+            name,
+            type,
+            price,
+            image,
+            description,
+            others,
+        });
+        res.status(200);
+        res.json('Product created in db');
+    }
 });
 
 module.exports = router;
