86e6fd23eafdacd5c800ef610a5a8c2b870a3cc0
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-20 20:56:51 +0100

    Commented register part

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,51 +4,53 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 
 // ====================================================== //
-// ====================== REGISTRO ====================== //
+// ===================== GENERAR JWT ==================== //
 // ====================================================== //
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
-const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
-    if (!email || !password) {
-        res.status(400);
-        throw new Error('Please fill all fields');
-    }
+// ====================================================== //
+// ====================== REGISTRO ====================== //
+// ====================================================== //
 
-    // Check if user exists
-    const userExists = await User.findOne({ email });
+// const registerUser = asyncHandler(async (req, res) => {
+//     const { email, password } = req.body;
+//     if (!email || !password) {
+//         res.status(400);
+//         throw new Error('Please fill all fields');
+//     }
 
-    if (userExists) {
-        res.status(400);
-        throw new Error('User already exists');
-    }
+//     // Check if user exists
+//     const userExists = await User.findOne({ email });
 
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+//     if (userExists) {
+//         res.status(400);
+//         throw new Error('User already exists');
+//     }
 
-    // Create user
-    const user = await User.create({
-        name,
-        email,
-        password: hashedPassword,
-    });
+//     // Hash password
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
 
-    if (user) {
-        res.status(201).json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user._id),
-        });
-    } else {
-        res.status(400);
-        throw new Error('Invalid user data');
-    }
-});
+//     // Create user
+//     const user = await User.create({
+//         email,
+//         password: hashedPassword,
+//     });
+
+//     if (user) {
+//         res.status(201).json({
+//             _id: user.id,
+//             email: user.email,
+//             token: generateToken(user._id),
+//         });
+//     } else {
+//         res.status(400);
+//         throw new Error('Invalid user data');
+//     }
+// });
 
 // ====================================================== //
 // ======================== LOGIN ======================= //
@@ -72,9 +74,5 @@ const loginUser = asyncHandler(async (req, res) => {
         throw new Error('Invalid credentials');
     }
 });
-
-// ====================================================== //
-// ===================== GENERAR JWT ==================== //
-// ====================================================== //
 
 module.exports = { registerUser, loginUser };
