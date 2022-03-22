072483ebaaca24a587d60c8f884e5f60bc421a5c
Daniel Gonzales <dani.gt75.dg@gmail.com>
2022-03-22 03:12:50 +0100

    app.jsx with AuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,33 +15,37 @@ import {
 import { Header, Footer, PrivateRoute } from './components';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './context';
 
 
 // import Bookings from './pages/Bookings/Bookings';
 
 function App() {
     return (
-        <div className="app">
-            <Toaster />
-            <Router>
-                <Header></Header>
-                <Routes>
-                    <Route path="/" element={<Navigate to="/home" />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/menu" element={<Menu />} />
-                    <Route path="/about" element={<Conocenos />} />
-                    <Route path="/locate" element={<Encuentranos />} />
-                    <Route path="/contact" element={<Contactanos />} />
-                    <Route path="/order" element={<Order />} />
-                    <Route path="/orderList" element={<OrderList />} />
-                    <Route path="/book" element={<Book />} />
-                    <Route path="/bookings" element={<PrivateRoute component={<Bookings />} />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </Router>
-            <Footer></Footer>
-        </div>
+        <AuthProvider>
+
+            <div className="app">
+                <Toaster />
+                <Router>
+                    <Header></Header>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/home" />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/menu" element={<Menu />} />
+                        <Route path="/about" element={<Conocenos />} />
+                        <Route path="/locate" element={<Encuentranos />} />
+                        <Route path="/contact" element={<Contactanos />} />
+                        <Route path="/order" element={<Order />} />
+                        <Route path="/orderList" element={<OrderList />} />
+                        <Route path="/book" element={<Book />} />
+                        <Route path="/bookings" element={<PrivateRoute component={<Bookings />} />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                </Router>
+                <Footer></Footer>
+            </div>
+        </AuthProvider>
     );
 }
 
