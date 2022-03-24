fcfd2b7bd0cd470d4c72d01691908fc00cde92c0
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-03-24 21:12:43 +0100

    Home carousel adapted

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -1,45 +1,73 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Autoplay } from "swiper";
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
 
-import "swiper/css";
-import "swiper/css/pagination";
-import styles from "./Carousel.module.scss";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import styles from './Carousel.module.scss';
 
 export default function Carousel() {
-  return (
-    <Swiper
-      className={styles.swiper}
-      modules={[Pagination, Autoplay]}
-      pagination={{
-        clickable: true,
-      }}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-    >
-      <SwiperSlide className={styles.swiperSlide}>
-        <img
-          className={styles.slideImg}
-          src="https://images.unsplash.com/photo-1572116469696-31de0f17cc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
-          alt=""
-        />
-      </SwiperSlide>
-      <SwiperSlide className={styles.swiperSlide}>
-        <img
-          className={styles.slideImg}
-          src="https://mldallasmagazine.com/get/files/image/galleries/cocktails-m-s-meeuwesen-unsplash.jpg"
-          alt=""
-        />
-      </SwiperSlide>
+    return (
+        <Swiper
+            className={styles.swiper}
+            modules={[Autoplay]}
+            pagination={{
+                clickable: true,
+            }}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}
+        >
+            <SwiperSlide className={styles.swiperSlide}>
+                <div className={styles.linkDiv}>
+                    <Link to="/menu" className={styles.link}>
+                        Carta
+                    </Link>
+                </div>
+                <img
+                    className={styles.slideImg}
+                    src="https://images.unsplash.com/photo-1572116469696-31de0f17cc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+                    alt=""
+                />
+            </SwiperSlide>
 
-      <SwiperSlide className={styles.swiperSlide}>
-        <img
-          className={styles.slideImg}
-          src="https://www.mixologist-bar.com/wp-content/uploads/2016/03/unnamed.png"
-          alt=""
-        />
-      </SwiperSlide>
-    </Swiper>
-  );
+            <SwiperSlide className={styles.swiperSlide}>
+                <div className={styles.linkDiv}>
+                    <Link to="/contact" className={styles.link}>
+                        Contactanos
+                    </Link>
+                </div>
+                <img
+                    className={styles.slideImg}
+                    src="https://images.unsplash.com/photo-1617524455617-ce1e266aa810?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80"
+                    alt=""
+                />
+            </SwiperSlide>
+            <SwiperSlide className={styles.swiperSlide}>
+                <div className={styles.linkDiv}>
+                    <Link to="/about" className={styles.link}>
+                        Conocenos
+                    </Link>
+                </div>
+                <img
+                    className={styles.slideImg}
+                    src="https://images.unsplash.com/photo-1566417713940-fe7c737a9ef2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2029&q=80"
+                    alt=""
+                />
+            </SwiperSlide>
+            <SwiperSlide className={styles.swiperSlide}>
+                <div className={styles.linkDiv}>
+                    <Link to="/locate" className={styles.link}>
+                        Encuentranos
+                    </Link>
+                </div>
+                <img
+                    className={styles.slideImg}
+                    src="https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                    alt=""
+                />
+            </SwiperSlide>
+        </Swiper>
+    );
 }
