7fffabdcca63fb79f18c6521d1cde90019fd4175
urii urii2 <urioleh@gmail.com>
2022-03-24 19:22:16 +0100

    FEAT: Front-> add emailForm to Contactanos; Back-> add nodemailer to index.js

--- frontend/src/pages/Contactanos/Contactanos.jsx
+++ frontend/src/pages/Contactanos/Contactanos.jsx
@@ -23,33 +23,33 @@ export default function Contactanos() {
 
     return (
         <div className={styles.container}>
-            <h2>Contáctanos</h2>
+            <h2 className={styles.title}>Contáctanos</h2>
             <h4>Envíanos un e-mail</h4>
             {!sent ? (
-                <div className="container p-4">
+                <div className="container p-4 ">
                 <div className="row">
-                    <div className="col-md-6 offset-md-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <form onSubmit={handleSend} variant="dark">
+                    <div className="col-md-8 offset-md-2">
+                        <div className="card bg-dark " >
+                            <div className="card-body p-1 ">
+                                <form onSubmit={handleSend} >
                 
-                                    <div className="form-group">
-                                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="name" className="form-control" placeholder="Name" autoFocus required/>
+                                    <div className="form-group m-4">
+                                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="name" className="form-control" placeholder="Nombre" autoFocus required/>
                                     </div>
 
-                                    <div className="form-group">
+                                    <div className="form-group m-4">
                                         <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} name="email" placeholder="Email" className="form-control" required/>
                                     </div>
                 
-                                    <div className="form-group">
-                                        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} name="phone" placeholder="Phone" className="form-control"/>
+                                    <div className="form-group m-4">
+                                        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} name="phone" placeholder="Teléfono de contacto" className="form-control"/>
                                     </div>
                 
-                                    <div className="form-group">
-                                        <textarea value={text} onChange={(e) => setText(e.target.value)}  name="message" rows="3" placeholder="Message" className="form-control" required/>
+                                    <div className="form-group m-4">
+                                        <textarea value={text} onChange={(e) => setText(e.target.value)}  name="message" rows="3" placeholder="Estimados dueños de Cocktails Club..." className="form-control" required/>
                                     </div>
                 
-                                    <button className="btn btn-primary btn-block">
+                                    <button className="btn btn-secondary btn-block mb-4 px-5 py-2" style={{'color': "var(--medium)"}} >
                                         Send
                                     </button>
                 
@@ -60,19 +60,19 @@ export default function Contactanos() {
                 </div>
                 </div>
             ) : (
-                <div>
-                    <h3>Email enviado</h3>
+                <div className={styles.enviado}>
+                    <h3 className={styles.enviadoTitle}>Mensaje enviado</h3>
                     <p>Muchas Gracias por ponerte en contacto con nosotros.</p>
                 </div>
             )}
 
             <h4>Nuestro número de teléfono</h4>
             <p>
-                <a className={styles.email} href={`tel:${+34684411802}`}>
+                <a className={styles.phone} href={`tel:${+34684411802}`}>
                     {" "}
                     Click to call{" "}
                 </a>
-                (+34684411802)
+                ( +34 684 411 802 )
             </p>
         </div>
     );
