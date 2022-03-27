c07513f2c85532a692e320d61462a4f95e8c8042
Carlos <carlos.casal@bootcamp-upgrade.com>
2022-03-27 16:50:41 +0200

    Booking post route modified

--- server/routes/booking.routes.js
+++ server/routes/booking.routes.js
@@ -7,7 +7,6 @@ router.post("/new", async (req, res, next) => {
   const bookingData = await Booking.findOne({ date: date, hour: hour });
   console.log(bookingData);
   if (!bookingData) {
-    console.log("Reserva disponible");
     let table = 1;
     const bookingCounter = await Booking.countDocuments();
     bookingCounter ? (table = bookingCounter + 1) : table;
@@ -19,13 +18,13 @@ router.post("/new", async (req, res, next) => {
         hour,
       });
       res.status(200);
-      res.json("New order send to db");
+      res.json("New booking send to db");
     } catch (error) {
-      res.json(`Error creating new order => ${error}`);
+      res.json(`Error creating new booking => ${error}`);
       res.status(400);
     }
   } else {
-    console.log("Reserva no disponible");
+    console.log("Current booking not available");
   }
 });
 
